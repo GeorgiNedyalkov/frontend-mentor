c37076f7e7b6a898316873fb9985f64221983765
Flu3nt <gnedyalkov94@gmail.com>
2023-08-12 18:04:34 +0300

    create mobile hero page

--- intro_section_dropdown_nav/intro-section-with-dropdown-navigation-main/client/src/App.jsx
+++ intro_section_dropdown_nav/intro-section-with-dropdown-navigation-main/client/src/App.jsx
@@ -1,9 +1,39 @@
+import HeroImage from "./assets/image-hero-mobile.png";
+import Logo from "./assets/logo.svg";
+import Audiophile from "./assets/client-audiophile.svg";
+import Databiz from "./assets/client-databiz.svg";
+import Maker from "./assets/client-maker.svg";
+import Meet from "./assets/client-meet.svg";
+
 function App() {
-  return (
-    <div>
-      <h1 className="text-3xl text-center">Hello</h1>
-    </div>
-  );
+    return (
+        <>
+            <div className="mb-10">
+                <header className="p-4 h-16 flex items-center justify-between">
+                    <img src={Logo} alt="" />
+                </header>
+                <img src={HeroImage} alt="hero image" />
+            </div>
+
+            <div className="p-4 flex flex-col items-center justify-center mb-4">
+                <h1 className="text-4xl font-bold mb-4">Make remote work</h1>
+
+                <p className="text-slate-500 mb-4">
+                    Get your team in sync, no matter your location. Streamline
+                    processes, create team rituals, and watch productivity soar.
+                </p>
+                <button className="rounded-xl bg-slate-950 text-white p-2 w-32">
+                    Learn more
+                </button>
+            </div>
+            <div className="flex items-center justify-between p-2">
+                <img className="w-16" src={Databiz} alt="" />
+                <img className="w-16" src={Audiophile} alt="" />
+                <img className="w-16" src={Maker} alt="" />
+                <img className="w-16" src={Meet} alt="" />
+            </div>
+        </>
+    );
 }
 
 export default App;
--- intro_section_dropdown_nav/intro-section-with-dropdown-navigation-main/style-guide.md
+++ intro_section_dropdown_nav/intro-section-with-dropdown-navigation-main/style-guide.md
@@ -4,24 +4,24 @@
 
 The designs were created to the following widths:
 
-- Mobile: 375px
-- Desktop: 1440px
+-   Mobile: 375px
+-   Desktop: 1440px
 
 ## Colors
 
 ### Neutral
 
-- Almost White: hsl(0, 0%, 98%)
-- Medium Gray: hsl(0, 0%, 41%)
-- Almost Black: hsl(0, 0%, 8%)
+-   Almost White: hsl(0, 0%, 98%)
+-   Medium Gray: hsl(0, 0%, 41%)
+-   Almost Black: hsl(0, 0%, 8%)
 
 ## Typography
 
 ### Body Copy
 
-- Font size (paragraph): 18px
+-   Font size (paragraph): 18px
 
 ### Font
 
-- Family: [Epilogue](https://fonts.google.com/specimen/Epilogue)
-- Weights: 500, 700
+-   Family: [Epilogue](https://fonts.google.com/specimen/Epilogue)
+-   Weights: 500, 700
